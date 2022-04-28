43df84238235715e031637546138d13684d03c2f
Andrea Severi <andrea.severi12@studio.unibo.it>
2022-04-28 14:26:17 +0200

    add scheduler task-activeness check

--- src/arduino/smart_cm/MenuTaskMain/Scheduler.cpp
+++ src/arduino/smart_cm/MenuTaskMain/Scheduler.cpp
@@ -1,8 +1,18 @@
 #include "Scheduler.h"
+#include <TimerOne.h>
+
+volatile bool timerFlag;
+
+void timerHandler(void){
+  timerFlag = true;
+}
 
 void Scheduler::init(int basePeriod){
   this->basePeriod = basePeriod;
-  timer.setupPeriod(basePeriod);  
+  timerFlag = false;
+  long period = 1000l*basePeriod;
+  Timer1.initialize(period);
+  Timer1.attachInterrupt(timerHandler);
   nTasks = 0;
 }
 
@@ -12,16 +22,16 @@ bool Scheduler::addTask(Task* task){
     nTasks++;
     return true;
   } else {
-    return false; 
+    return false;
   }
 }
 
-
-  
 void Scheduler::schedule(){
-  timer.waitForNextTick();
+  while (!timerFlag){}
+  timerFlag = false;
+
   for (int i = 0; i < nTasks; i++){
-    if (taskList[i]->updateAndCheckTime(basePeriod)){
+    if (taskList[i]->isActive() && taskList[i]->updateAndCheckTime(basePeriod)){
       taskList[i]->tick();
     }
   }
